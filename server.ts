407b6d3d928da41b801fa3694a91340dde9e7a9a
emeraldlinks <josephchukwumachristopher@gmail.com>
2025-10-27 18:26:00 +0100

    l

--- server.ts
+++ server.ts
@@ -1,6 +1,13 @@
+import { App } from "rivra";
 import { StartServer } from "rivra/server"
-
+let app: App
 (async () => {
   const app = await StartServer();
   // app.register(...) // register plugins and add custom instance behaviours.
 })();
+
+
+export default async function handler(req: any, res: any) {
+  await app.ready()
+  app.server.emit('request', req, res)
+}
