9feae22b82d7923478ddae851cfbc66c56a42392
emeraldlinks <josephchukwumachristopher@gmail.com>
2025-10-28 09:49:08 +0100

    fixing vercel

--- server.ts
+++ server.ts
@@ -1,6 +1,15 @@
-import { StartServer } from "rivra/server"
+import { App } from "rivra";
+import { StartServer } from "rivra/server";
+
+let app: App;
 
 (async () => {
-  const app = await StartServer();
-  // app.register(...) // register plugins and add custom instance behaviours.
+  app = await StartServer();
+  // You can register plugins or routes here if needed
+  // await app.register(...)
 })();
+
+export default async function handler(req: any, res: any) {
+  await app.ready();
+  app.server.emit("request", req, res);
+}
